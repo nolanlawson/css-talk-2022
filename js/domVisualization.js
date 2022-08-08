578045d528d94d041333e617f1e94dd28d768813
Nolan Lawson <nolan@nolanlawson.com>
2022-08-07 20:55:21 -0700

    this is not working

--- js/domVisualization.js
+++ js/domVisualization.js
@@ -14,28 +14,27 @@ customElements.define('dom-visualization', class extends HTMLElement {
     .root, .root * {
       display: flex;
       flex-direction: column;
-      height: 100%;
+      flex: 1;
       --width: calc(var(--root-width) / var(--max-tree-depth));
       width: var(--width);
-      left: var(--width);
-      position: relative;
+      position: absolute;
+      left: 0;
       font-size: 36px;
     }
     .tree {
-      position: relative;
       width: 100%;
       height: 100%;
+      position: relative;
     }
     .root {
       --tree-depth: 0;
       transform: translateX(0);
-      left: 0;
+      --num-siblings: 1;
     }
     .root::after, .root *::after {
        content: attr(data-content);
        border: 2px solid black;
        border-radius: 100%;
-       position: absolute;
        left: 20px;
        right: 20px;
        --height: calc(var(--root-height) / var(--max-tree-width));
@@ -74,17 +73,19 @@ customElements.define('dom-visualization', class extends HTMLElement {
       let maxDepth = 0
       let maxWidth = 0
 
-      const setDepth = (element, depth, width) => {
+      const setDepth = (element, depth, width, i) => {
         maxDepth = Math.max(depth, maxDepth)
         maxWidth = Math.max(width, maxWidth)
         element.style.setProperty('--tree-depth', depth)
-        for (const child of element.children) {
-          setDepth(child, depth + 1, element.children.length * width)
-        }
+        element.style.setProperty('--num-siblings', width)
+        element.style.setProperty('--sibling-order', i)
+        Array.from(element.children).forEach((child, i) => {
+          setDepth(child, depth + 1, element.children.length * width, i)
+        })
       }
 
       for (const element of root.children) {
-        setDepth(element, 1, root.children.length)
+        setDepth(element, 1, root.children.length, 0)
       }
       this.style.setProperty('--max-tree-depth', maxDepth + 1)
       this.style.setProperty('--max-tree-width', maxWidth)
